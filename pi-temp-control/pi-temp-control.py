15727754b9d60ec5bf1f7dfd63366fdd766fa171
Eric Baggen <eric.baggen@gmail.com>
2019-12-04 19:42:08 -0800

    Implemented PID control

--- pi-temp-control/pi-temp-control.py
+++ pi-temp-control/pi-temp-control.py
@@ -3,35 +3,51 @@
 import subprocess
 import time
 import RPi.GPIO as GPIO
+from simple_pid import PID
 
 ON_THRESHOLD = 45
 OFF_THRESHOLD = 40
 SLEEP_INTERVAL = 1
-GPIO_PIN = 17
+GPIO_PIN = 18
+
 
 def get_temp():
-  output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
-  temp_str = output.stdout.decode()
-  try:
-    return float(temp_str.split('=')[1].split('\'')[0])
-  except (IndexError, ValueError):
-    raise RuntimeError('Could not parse temperature output')
+    output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
+    temp_str = output.stdout.decode()
+    try:
+        return float(temp_str.split('=')[1].split('\'')[0])
+    except (IndexError, ValueError):
+        raise RuntimeError('Could not parse temperature output')
 
 
 if __name__ == '__main__':
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(GPIO_PIN, GPIO.OUT)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_PIN, GPIO.OUT)
+    pwm = GPIO.PWM(GPIO_PIN, 1000)
+    pwm.start()
 
-  try:
-    while True:
-      temp = get_temp()
-      if temp > ON_THRESHOLD:
-        GPIO.output(GPIO_PIN, GPIO.HIGH)
-      elif temp < OFF_THRESHOLD:
-        GPIO.output(GPIO_PIN, GPIO.LOW)
-      time.sleep(SLEEP_INTERVAL)
+    pid = PID(setpoint=50)
+    pid.sample_time = 0.1  # update every 0.1 seconds
 
-  except KeyboardInterrupt:
-    GPIO.cleanup()
-    print("exit")
+    try:
+        while True:
+            # Basic switching implementation
+            '''
+            temp = get_temp()
+            if temp > ON_THRESHOLD:
+                GPIO.output(GPIO_PIN, GPIO.HIGH)
+            elif temp < OFF_THRESHOLD:
+                GPIO.output(GPIO_PIN, GPIO.LOW)
+            '''
 
+            # PID implementation
+            pv = get_temp()
+            cv = pid(pv)
+            pwm.ChangeDutyCycle(cv)
+
+            time.sleep(SLEEP_INTERVAL)
+
+    except KeyboardInterrupt:
+        pwm.stop()
+        GPIO.cleanup()
+        print("exit")
