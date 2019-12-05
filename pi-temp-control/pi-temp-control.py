cdc41665464099b6d0a759e705887cbc34063b58
Eric Baggen <eric.baggen@gmail.com>
2019-12-04 20:59:08 -0800

    Added config file

--- pi-temp-control/pi-temp-control.py
+++ pi-temp-control/pi-temp-control.py
@@ -3,6 +3,7 @@
 import subprocess
 import time
 import RPi.GPIO as GPIO
+import toml
 from simple_pid import PID
 
 ON_THRESHOLD = 45
@@ -21,6 +22,9 @@ def get_temp():
 
 
 if __name__ == '__main__':
+    # Load configuration file
+    config = toml.load('config.toml')
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GPIO_PIN, GPIO.OUT)
     pwm = GPIO.PWM(GPIO_PIN, 1000)
