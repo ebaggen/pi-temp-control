f2c54fd822ca7439b97e99056506a15c0ea7357a
Eric Baggen <eric.baggen@gmail.com>
2019-12-13 15:18:26 -0800

    Added simulation for testing on non Rasp Pi systems (set T or F in config file)

--- src/pifan.py
+++ src/pifan.py
@@ -5,36 +5,55 @@ import sys
 import time
 import RPi.GPIO as GPIO
 import toml
+from random import random
 from simple_pid import PID
-
+2
 SLEEP_INTERVAL = 0.1
 
 
 def get_temp():
-    output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
-    temp_str = output.stdout.decode()
-    try:
-        return float(temp_str.split('=')[1].split('\'')[0])
-    except (IndexError, ValueError):
-        raise RuntimeError('Could not parse temperature output')
+    if config['control']['simulate']:
+        temp = random()
+    else:
+        output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
+        temp_str = output.stdout.decode()
+        try:
+            temp = float(temp_str.split('=')[1].split('\'')[0])
+        except (IndexError, ValueError):
+            raise RuntimeError('Could not parse temperature output')
+    return temp
+
+
+def stop():
+    print('Exiting....')
+    GPIO.cleanup()
+    sys.exit()
 
 
 def pid_control():
-    while True:
-        pv = get_temp()
-        cv = pid(pv) * -1  # Invert cv to make a reverse acting loop
-        pwm.ChangeDutyCycle(cv)
-        time.sleep(SLEEP_INTERVAL)
+    try:
+        while True:
+            pv = get_temp()
+            cv = pid(pv) * -1  # Invert cv to make a reverse acting loop
+            pwm.ChangeDutyCycle(cv)
+            time.sleep(SLEEP_INTERVAL)
+
+    except KeyboardInterrupt:
+        stop()
 
 
 def on_off_control():
-    while True:
-        pv = get_temp()
-        if pv >= config['control']['on_off']['on_temperature']:
-            pass
-        elif pv <= config['control']['on_off']['off_temperature']:
-            pass
-        time.sleep(SLEEP_INTERVAL)
+    try:
+        while True:
+            pv = get_temp()
+            if pv >= config['control']['on_off']['on_temperature']:
+                pass
+            elif pv <= config['control']['on_off']['off_temperature']:
+                pass
+            time.sleep(SLEEP_INTERVAL)
+    except KeyboardInterrupt:
+        pwm.stop()
+        stop()
 
 
 if __name__ == '__main__':
@@ -60,14 +79,8 @@ if __name__ == '__main__':
                   sample_time=config['control']['pid']['sample_time'],
                   output_limits=(-100, 0))
 
-    try:
-        if mode == 'on_off':
-            on_off_control()
-        elif mode == 'pid':
-            pid_control()
-
-    except KeyboardInterrupt:
-        print('Exiting....')
-        pwm.stop()
-        GPIO.cleanup()
-        sys.exit()
+    # Kick off mode-dependent function
+    if mode == 'on_off':
+        on_off_control()
+    elif mode == 'pid':
+        pid_control()
